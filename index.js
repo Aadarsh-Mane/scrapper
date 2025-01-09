68de4047bf7d82b31a34b735f3de0e20d7d14c54
Aadarsh-Mane <onlyaddy68@gmail.com>
2025-01-10 02:56:39 +0530

    oo

--- index.js
+++ index.js
@@ -53,6 +53,19 @@ newspapers.forEach((newspaper) => {
 app.get("/", (req, res) => {
   res.json("Welcome to my Scrapper API");
 });
+app.get("/proxy-image", async (req, res) => {
+  const imageUrl = req.query.url;
+  try {
+    const response = await axios.get(imageUrl, {
+      responseType: "arraybuffer",
+    });
+    const contentType = response.headers["content-type"];
+    res.set("Content-Type", contentType);
+    res.send(response.data);
+  } catch (error) {
+    res.status(500).send("Error fetching image");
+  }
+});
 
 app.get("/news", (req, res) => {
   res.json(articles);
